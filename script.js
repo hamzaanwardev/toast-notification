27ebbeaa9921d2eca0c78f8b2e18a3d7fbecf1a4
Hamza Anwar <88555920+hamzaanwardev@users.noreply.github.com>
2021-09-15 23:53:12 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 const button = document.getElementById('button');
 const toasts = document.getElementById('toasts');
+
+const messages = [
+    'Message One',
+    'Message Two',
+    'Message Three',
+    'Message Four'
+];
+button.addEventListener('click', () => createNotifications ());
+
+function createNotifications (message = null, type = null) {
+    const notif = document.createElement('div');
+    notif.classList.add('toast');
+    notify.classList.add(type ? type : ''info);
+    notif.innerText = message ? message : getRandomMessages();
+    toasts.appendChild(notif)
+
+    setTimeout ( () => {
+        notif.remove()
+    }, 3000)
+}
+function getRandomMessages () {
+    return messages[Math.floor (Math.random()) * messages.length ]
+};
